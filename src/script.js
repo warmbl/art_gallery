701c586dec1715856e58a6140d97e8f5d201bdaf
Ремизова Ирина <w.warmbl@gmail.com>
2022-06-09 00:32:04 +0500

    Fix animation

--- src/script.js
+++ src/script.js
@@ -1,80 +1,65 @@
 let open = document.querySelector(".open-gallery");
 let close = document.querySelector(".close-gallery");
-
 let center_menu = document.querySelector(".center-menu");
-let text = document.querySelector(".headline");
 let menu_item;
-
 let can_open = true;
 
-//let cln;
-
-//let cln = document.querySelectorAll(".js-clone");
-//console.log(cln);
-
-//let clons = sldr.querySelectorAll(".js-clone");
-//console.log(clons);
-
+// Функция для анимации массива элементов с задержкой
 const delayLoop = (fn, delay) => {
     return (x, i) => {
         setTimeout(() => {
             fn(x);
-        }, i * delay);
+        }, i * delay - 30 * i);
     };
 };
 
 const appear = e => {
-    e.style.visibility = "visible";
+    setTimeout(() => {
+        e.style.visibility = "visible";
+    }, 100);
     e.style.animation = "appearance 0.3s linear 1";
 };
 
 const disappear = e => {
-    e.style.animation = "disappearance 0.3s linear 1";
+    e.style.visibility = "hidden";
     setTimeout(() => {
-        e.style.visibility = "hidden";
-    }, 100);
+        e.style.animation = "disappearance 0.3s linear 1";
+    }, 300);
 };
 
-//console.log(clons);
 function open_gall() {
     if (can_open) {
-        text.style.visibility = "visible";
-        text.style.animation = "up 0.3s linear 1";
         //
         menu_item.forEach(delayLoop(appear, 100));
         console.log(menu_item);
         setTimeout(() => {
             center_menu.style.visibility = "visible";
-        }, 500);
+        }, 400);
 
-        open.style.display = "none";
-        close.style.display = "inline";
-        can_open = false;
+        setTimeout(() => {
+            open.style.display = "none";
+            close.style.display = "inline";
+            can_open = false;
+        }, 700);
     } else return;
 }
 
 function close_gall() {
     if (!can_open) {
-        center_menu.style.visibility = "hidden";
-        //
+        menu_item.forEach(delayLoop(disappear, 50));
         setTimeout(() => {
-            text.style.animation = "down 0.4s linear 1";
-        }, 100);
-        setTimeout(() => {
-            text.style.visibility = "hidden";
+            center_menu.style.visibility = "hidden";
         }, 500);
-        menu_item.forEach(delayLoop(disappear, 100));
-
-        open.style.display = "inline";
-        close.style.display = "none";
-        can_open = true;
+        setTimeout(() => {
+            open.style.display = "inline";
+            close.style.display = "none";
+            can_open = true;
+        }, 200);
     } else return;
 }
 
 function load() {
     menu_item = center_menu.querySelectorAll(".slider-item");
-    //console.log(menu_item);
-    //cln.style.visibility = "hidden";
     open.addEventListener("click", open_gall);
     close.addEventListener("click", close_gall);
 }

51e7dd828efd92fb38ead58eeb2e417d54309edb
Ремизова Ирина <w.warmbl@gmail.com>
2022-06-17 18:47:58 +0500

    Add preloader

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ let back = document.querySelector("body");
 let blackout = document.querySelector(".blackout");
 let headline = document.querySelector(".headline");
 let parallax = document.querySelector(".back-parallax");
+let bg_image = document.querySelector(".bg-image");
 let layer;
 let can_open = true;
 let cards;
@@ -71,52 +72,83 @@ function addElement(e) {
     }, 1);
 }
 
-function changeBackground(folder, count) {
-    addDiv.style.backgroundImage = `url(./images/${folder}.jpg)`;
+function changeBackground(count) {
+    addDiv.style.backgroundImage = `url(./images/${count}.jpg)`;
     setTimeout(() => {
         parallax.innerHTML = "";
-        for (let i = 1; i <= count; i++) {
+        for (let i = 0; i < preload[count - 1].length; i++) {
+            //console.log(preload[count - 1].length);
             layer = document.createElement("img");
             layer.classList.add("parallax");
-            layer.src = `./imagesHD/test${folder}/${i}.png`;
+            //layer.src = `./imagesHD/test${id}/${i}.png`;
+            layer.src = preload[count - 1][i];
             parallax.appendChild(layer);
         }
         close_gall();
-        //
+        bg_image.style.backgroundImage = `url(./images/${count}.jpg)`;
     }, 1000);
     setTimeout(() => {
         back.lastChild.remove();
     }, 2000);
 }
-/*
+
+var preload = [
+    [
+        "./imagesHD/test1/1.png",
+        "./imagesHD/test1/2.png",
+        "./imagesHD/test1/3.png",
+        "./imagesHD/test1/4.png",
+    ],
+    [
+        "./imagesHD/test2/1.png",
+        "./imagesHD/test2/2.png",
+        "./imagesHD/test2/3.png",
+        "./imagesHD/test2/4.png",
+    ],
+    [
+        "./imagesHD/test3/1.png",
+        "./imagesHD/test3/2.png",
+        "./imagesHD/test3/3.png",
+        "./imagesHD/test3/4.png",
+        "./imagesHD/test3/5.png",
+    ],
+    ["./imagesHD/test4/1.png", "./imagesHD/test4/2.png", "./imagesHD/test4/3.png"],
+    [
+        "./imagesHD/test5/1.png",
+        "./imagesHD/test5/2.png",
+        "./imagesHD/test5/3.png",
+        "./imagesHD/test5/4.png",
+    ],
+    [
+        "./imagesHD/test6/1.png",
+        "./imagesHD/test6/2.png",
+        "./imagesHD/test6/3.png",
+        "./imagesHD/test6/4.png",
+        "./imagesHD/test6/5.png",
+    ],
+];
 function preloader() {
-    var preload = [
-        "./images/1.jpg",
-        "./images/2.jpg",
-        "./images/3.jpg",
-        "./images/4.jpg",
-        "./images/5.jpg",
-        "./images/6.jpg",
-    ];
     var images = [];
     for (var i = 0; i < preload.length; i++) {
-        images[i] = new Image();
-        images[i].src = preload[i];
+        for (var j = 0; j < preload[i].length; j++) {
+            images[j] = new Image();
+            images[j].src = preload[i][j];
+        }
     }
-    alert("Изображения загружены");
+    //console.log(preload);
 }
-*/
+preloader();
+//console.log(preload[2]);
 
 async function load() {
-    //preloader();
     // Изначально загружается первый арт (test1)
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 0; i <= 3; i++) {
         layer = document.createElement("img");
         layer.classList.add("parallax");
-        layer.src = `./imagesHD/test1/${i}.png`;
+        //layer.src = `./imagesHD/test1/${i}.png`;
+        layer.src = `${preload[0][i]}`;
         parallax.appendChild(layer);
     }
-
     cards = document.querySelectorAll(".card");
     cards.forEach(crd => {
         crd.addEventListener("click", addElement);
@@ -124,22 +156,22 @@ async function load() {
             let id_back = crd.id;
             switch (id_back) {
                 case "1":
-                    changeBackground(id_back, 4);
+                    changeBackground(id_back);
                     break;
                 case "2":
-                    changeBackground(id_back, 4);
+                    changeBackground(id_back);
                     break;
                 case "3":
-                    changeBackground(id_back, 5);
+                    changeBackground(id_back);
                     break;
                 case "4":
-                    changeBackground(id_back, 3);
+                    changeBackground(id_back);
                     break;
                 case "5":
-                    changeBackground(id_back, 4);
+                    changeBackground(id_back);
                     break;
                 case "6":
-                    changeBackground(id_back, 5);
+                    changeBackground(id_back);
                     break;
                 default:
                     back.style.backgroundColor = "red";
